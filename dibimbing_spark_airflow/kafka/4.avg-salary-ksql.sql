3280ebec9d5980aa76f9272c32868429f3e947cf
Rahmadiyan Muhammad <riansshole124@gmail.com>
2024-11-15 23:18:00 +0700

    .

--- dibimbing_spark_airflow/kafka/4.avg-salary-ksql.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE avg_salary_table AS
-SELECT
-    department,
-    AVG(salary) AS avg_salary
-FROM employee_stream
-GROUP BY department
-EMIT CHANGES;
--- dibimbing_spark_airflow/scripts/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[   $(docker ps --filter name=dataeng* -aq) ]]; then
-    echo 'Stopping Container...'
-    docker ps --filter name=dataeng* -aq | xargs docker stop
-    echo 'All Container Stopped...'
-else
-    echo "All Cleaned UP!"
-fi
